bedf49b78167ff38d08f8f144c5f8bff4b782a2c
ayushsindhwal <ayushsindhwal1@gmail.com>
2022-01-17 12:09:51 +0530

    'adding more css'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Grid } from "@mui/material";
 import Samplecard from "./components/card/samplecard";
+import products from "./utils/products.json"
+
 function App() {
+
   return (
     <div className="App">
-      <Samplecard/>
+    <Grid container spacing={{ xs: 2, md: 2}} columns={{ xs: 4, sm: 8, md: 12 }}>
+    {products.products.map((product, index)=>(
+      <Grid item xs={2} sm={3} md={3} key={product.id}>
+      <Samplecard product={product} index={index}  />
+      </Grid>
+      ))}
+    </Grid>
     </div>
   );
 }
--- src/components/card/samplecard.jsx
+++ src/components/card/samplecard.jsx
@@ -4,36 +4,36 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import styles from './sample.module.css';
 import { style } from '@mui/system';
-
-const Samplecard = () => {
-
+const Samplecard = ({ product, index }) => {
+const {cover, price, name, priceSale, tags, status} = product
   return (
-    <Card sx={{ maxWidth: 325, width: 300, borderRadius: "18px" }}>
+    <Card sx={{ maxWidth: 325, width: 280, borderRadius: "18px"}}>
       <ImageListItem>
         <img
-          src="image/product_1.jpg"
+          src={cover}
           alt='hh'
           loading="lazy"
-          style={{ width: '100%' }}
+          style={{ width: '100%', height:'300px' }}
         />
         <ImageListItemBar
           sx={{
             background:
               'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
               'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+            display: (product.status === '') ? "none" : "block"
           }}
           position="top"
           actionIcon={
-            <span className={styles.saleLogo}>
-                Sale
+            <span className={styles.saleLogo} >
+             {product.status}
             </span>
           }
           actionPosition="right"
         />
       </ImageListItem>
       <div className={styles.itemName}>
-        <Link href="#" underline="hover" alignItems='center' color='#000' variant='subtitle2' sx={{ marginLeft: '3px' }} >
-          {'Nike Air Force 1 NDESTRUKT'}
+        <Link href="#" underline="hover" alignItems='center' color='#000' variant='subtitle2' className={styles.links} >
+          {name}
         </Link> 
         <div className={styles.footerCard}>
 
@@ -42,8 +42,11 @@ const Samplecard = () => {
       <div className={styles.colorBlack}></div>
       </span>
           <div className={styles.price}>
-          <Typography variant="body1" gutterBottom component="div">
-        $616
+          <span className={styles.salePrice}>
+          {priceSale ? `$${priceSale}` : null }          
+          </span>
+          <Typography variant="body1" gutterBottom component="div" sx={{fontWeight:600, marginLeft:0.5}} >
+        ${price}
       </Typography>
           </div>
           </div>
